5a6c37fad7d78e45faf2f2b856b52e292fbefa70
Jamie897 <jamiedanielle456@gmail.com>
2023-09-08 14:25:45 -0500

    Task 10

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const myArray = [];
+    for (const idx of array) {
+      const value = idx;
+      myArray.push(appendString + value);
+    }
+  
+    return myArray;
+  }
